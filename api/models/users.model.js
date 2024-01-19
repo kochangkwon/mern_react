c4b3a3ef70091637a592ed80a4550765a92d16c3
kochangkwon <inpenet69@gmail.com>
2024-01-19 20:27:40 +0900

    create user model

---
+++ api/models/users.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import mongoose from 'mongoose';
+
+const mongooseSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const user = mongoose.model('User', mongooseSchema);
+
+export default user;
