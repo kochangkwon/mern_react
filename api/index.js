a691c13913768453c234e93fc426d2e6c5eba1b3
kochangkwon <inpenet69@gmail.com>
2024-01-19 19:22:54 +0900

    mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('접속되었습니다.');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
